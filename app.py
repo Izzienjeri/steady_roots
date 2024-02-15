a941a19586bedd332dcc86f3b00bee3d6748bf0b
Aisha-Yusuf <ayshaofficiel@gmail.com>
2024-02-15 13:35:20 +0300

    get method added

--- app.py
+++ app.py
@@ -2,3 +2,17 @@ from models import db, Mentor, Mentee
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+migrate = Migrate(app,db)
+db.init_app(app)
+api = Api(app)
+
+class Mentor(Resource):
+    def get(self, mentor_id):
+        mentor = Mentor.query.get(mentor_id)
+        if mentor:
+            return {'id': mentor.id, 'description': mentor.description, 'skill_id': mentor.skill_id, 'user_id': mentor.user_id}
+        else:
+            return {'message': 'Mentor not found'}, 404
