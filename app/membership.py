ba33aa73b62e48973576daa9f7112a2bbc8424e9
izzie <izzienjeri12@gmail.com>
2024-02-15 15:32:38 +0300

    fix: placed resources into blueprints

---
+++ app/membership.py
@@ -0,0 +1,60 @@
+from flask import Blueprint
+from flask_restful import Api, Resource, reqparse
+from models import db,Membership
+
+membership_bp=Blueprint('membership_blueprint',__name__)
+api=Api(membership_bp)
+
+
+
+membership_parser = reqparse.RequestParser()
+membership_parser.add_argument('amount', type=float, required=True, help='Membership amount is required')
+membership_parser.add_argument('membership', type=bool, required=True, help='Membership status is required')
+membership_parser.add_argument('expires', type=str, required=True, help='Expiration date is required')
+membership_parser.add_argument('user_id', type=str, required=True, help='User ID is required')
+
+
+class MembershipListResource(Resource):
+    def get(self):
+        memberships = Membership.query.all()
+        return [{'id': membership.membership_id, 'amount': membership.amount, 'membership': membership.membership, 'expires': membership.expires} for membership in memberships]
+
+    def post(self):
+        data = membership_parser.parse_args()
+        new_membership = Membership(amount=data['amount'], membership=data['membership'], expires=data['expires'], user_id=data['user_id'])
+        db.session.add(new_membership)
+        db.session.commit()
+        return {'message': 'Membership created successfully'}, 201
+
+class MembershipResource(Resource):
+    def get(self, membership_id):
+        membership = Membership.query.get(membership_id)
+        if membership:
+            return {'id': membership.membership_id, 'amount': membership.amount, 'membership': membership.membership, 'expires': membership.expires}
+        else:
+            return {'message': 'Membership not found'}, 404
+
+    def patch(self, membership_id):
+        data = membership_parser.parse_args()
+        membership = Membership.query.get(membership_id)
+        if membership:
+            membership.amount = data['amount']
+            membership.membership = data['membership']
+            membership.expires = data['expires']
+            membership.user_id = data['user_id']
+            db.session.commit()
+            return {'message': 'Membership updated successfully'}, 200
+        else:
+            return {'message': 'Membership not found'}, 404
+
+    def delete(self, membership_id):
+        membership = Membership.query.get(membership_id)
+        if membership:
+            db.session.delete(membership)
+            db.session.commit()
+            return {'message': 'Membership deleted successfully'}, 200
+        else:
+            return {'message': 'Membership not found'}, 404
+
+api.add_resource(MembershipListResource, '/memberships')
+api.add_resource(MembershipResource, '/memberships/<string:membership_id>')        
---
+++ app/mentor.py
@@ -0,0 +1,57 @@
+from flask import Blueprint
+from flask_restful import Api, Resource, reqparse
+from models import db,Mentor
+
+mentor_bp=Blueprint('mentor_blueprint',__name__)
+api=Api(mentor_bp)
+
+
+mentor_parser = reqparse.RequestParser()
+mentor_parser.add_argument('description', type=str, required=True, help='Mentor description is required')
+mentor_parser.add_argument('skill_id', type=str, required=True, help='Skill ID is required')
+mentor_parser.add_argument('user_id', type=str, required=True, help='User ID is required')
+
+
+class MentorListResource(Resource):
+    def get(self):
+        mentors = Mentor.query.all()
+        return [{'id': mentor.mentor_id, 'description': mentor.description, 'skill_id': mentor.skill_id, 'user_id': mentor.user_id} for mentor in mentors]
+
+    def post(self):
+        data = mentor_parser.parse_args()
+        new_mentor = Mentor(description=data['description'], skill_id=data['skill_id'], user_id=data['user_id'])
+        db.session.add(new_mentor)
+        db.session.commit()
+        return {'message': 'Mentor created successfully'}, 201
+
+class MentorResource(Resource):
+    def get(self, mentor_id):
+        mentor = Mentor.query.get(mentor_id)
+        if mentor:
+            return {'id': mentor.mentor_id, 'description': mentor.description, 'skill_id': mentor.skill_id, 'user_id': mentor.user_id}
+        else:
+            return {'message': 'Mentor not found'}, 404
+
+    def patch(self, mentor_id):
+        data = mentor_parser.parse_args()
+        mentor = Mentor.query.get(mentor_id)
+        if mentor:
+            mentor.description = data['description']
+            mentor.skill_id = data['skill_id']
+            mentor.user_id = data['user_id']
+            db.session.commit()
+            return {'message': 'Mentor updated successfully'}, 200
+        else:
+            return {'message': 'Mentor not found'}, 404
+
+    def delete(self, mentor_id):
+        mentor = Mentor.query.get(mentor_id)
+        if mentor:
+            db.session.delete(mentor)
+            db.session.commit()
+            return {'message': 'Mentor deleted successfully'}, 200
+        else:
+            return {'message': 'Mentor not found'}, 404
+
+api.add_resource(MentorListResource, '/mentors')
+api.add_resource(MentorResource, '/mentors/<string:mentor_id>')
